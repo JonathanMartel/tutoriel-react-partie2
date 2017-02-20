5b37a4d18103ba4082bafeef3ede18df62212751
Jonathan Martel <jonathmartel@gmail.com>
2017-02-19 21:00:43 -0500

    Fin du tutoriel React partie 2

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './src/App/App.jsx';
 
-const element = <h1>Allo le monde</h1>;
-
-ReactDOM.render(element, document.getElementById("app"));
+ReactDOM.render(<App nom="toto le magicien" />, document.getElementById("app"));
